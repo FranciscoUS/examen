309f67e510853a6cd02a30e8455fea160c950a32
FranciscoUS <franciscohidalgoplaza@gmail.com>
2015-11-19 13:42:09 +0100

    Revert "Renombrado del metodo perimeter a perimetro"
    This reverts commit dadf321583ec9b4d21e794d2d8dfc4ba134ef58b.

--- pentagono.py
+++ pentagono.py
@@ -2,7 +2,12 @@ class pentagono:
 	def __init__(self, lado):
 		self.lado = lado
 
+<<<<<<< HEAD
         def perimetro(self):
    	         return 0
 
+=======
+        def perimeter(self):
+   	        return 5 + self.lado
+>>>>>>> parent of dadf321... Renombrado del metodo perimeter a perimetro
 
